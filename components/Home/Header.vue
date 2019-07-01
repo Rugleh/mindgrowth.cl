1bf4fd0c56d654935846ba9b20bf78a44f5cb514
Diego Rubilar <diegorubilartagle@gmail.com>
2019-07-01 17:22:03 -0400

    Update Header.vue

--- components/Home/Header.vue
+++ components/Home/Header.vue
@@ -34,15 +34,22 @@
       </form> -->
 
       <form name="contact" method="POST" data-netlify="true">
-         <p>
-            <label><input type="text" name="name" placeholder="Su nombre"/></label>   
-         </p>
-         <p>
-            <label><input type="email" name="email" placeholder="Correo" /></label>
-         </p>
-         <p>
-            <button class="btn btn-outline-primary w-100" type="submit">Enviar</button>
-         </p>
+         <div class="form-row">
+            <div class="div-form col-md-4 col-sm-12">
+               <label><input class="form-control" type="text" name="name" placeholder="Su nombre"/></label>   
+            </div>
+            <div class="div-form col-md-4 col-sm-12">
+               <label><input class="form-control" type="email" name="email" placeholder="Correo" required/></label>
+            </div>
+         </div>
+         <div class="form-row">
+            <div class="div-form col-md-4 col-sm-12">
+               <label><input class="form-control" type="text" name="phone" placeholder="Teléfono"/></label>
+            </div>
+            <div class="div-form col-md-4 col-sm-12">
+               <button class="btn btn-outline-primary w-100" type="submit">Enviar</button>
+            </div>
+         </div>
       </form>
 
    </div>
