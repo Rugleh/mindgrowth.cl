180a7aefe6f197a21157e502aca1b9a3ab04a8f2
Diego Rubilar <diegorubilartagle@gmail.com>
2019-07-08 00:26:08 -0400

    updated npm and installed jquery?

--- static/sw.js
+++ static/sw.js
@@ -2,35 +2,39 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/4aff4aaa6a2aa631bad8.js",
-    "revision": "9ca7011feaa8cbc4689f263cffe077ae"
+    "url": "/_nuxt/088fbf3cd85e06bd6171.js",
+    "revision": "abd958b1c121f84d7190219517cb511d"
   },
   {
-    "url": "/_nuxt/50542e649979c06875fd.js",
-    "revision": "96d2e17ea15a797d5e519640367d7e39"
+    "url": "/_nuxt/12246042b5bd08cda065.js",
+    "revision": "6686b1ec6e9224b08deecfa5300d25ca"
   },
   {
-    "url": "/_nuxt/827619dbc52eb698a8af.js",
-    "revision": "871f65c3f985900af82a98f1d2f3a89f"
+    "url": "/_nuxt/206d460835c946c5c1bc.js",
+    "revision": "6021b26f02e5a26d03a8fe734bdbc895"
   },
   {
-    "url": "/_nuxt/8bbc88a5a8818307767b.js",
-    "revision": "8db97e21499e78f1a85b26832509bf3a"
+    "url": "/_nuxt/3fc68019a0bd52ec3143.js",
+    "revision": "20a929ae136cba375c21370f3dbdb506"
   },
   {
-    "url": "/_nuxt/9c0da14335fc316f176f.js",
-    "revision": "099cf51da9d5567d6e28b2f9a4138590"
+    "url": "/_nuxt/4e5c494de1cfefd88d12.js",
+    "revision": "de427cdfa806a4d0bf57fde9d0e61eec"
   },
   {
-    "url": "/_nuxt/b6aa4e056f3041584902.js",
-    "revision": "6a942a2e70b63eebaf6f39739a58ddb5"
+    "url": "/_nuxt/5853a9dc36511a2ef4ad.js",
+    "revision": "865f8f483730ab80aa8e11c48b94ffa0"
   },
   {
-    "url": "/_nuxt/d7dc384e1c6f792c8bea.js",
-    "revision": "74d636b01c985694fa7247e0aabbfae5"
+    "url": "/_nuxt/c7be706d9ee954866bd8.js",
+    "revision": "73dd178d998ac73b5e7bdbe08a12ff49"
+  },
+  {
+    "url": "/_nuxt/d0c13a05b282c51ee702.js",
+    "revision": "2e182bb4a663de947886f91e7138b6de"
   }
 ], {
-  "cacheId": "nuxt-netlifycms-boilerplate",
+  "cacheId": "mindgrowth.cl",
   "directoryIndex": "/",
   "cleanUrls": false
 })
