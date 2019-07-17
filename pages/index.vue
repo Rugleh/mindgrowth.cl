4c37aa348767d6cdae6bf7bbb1f537eb00f6180f
Diego Rubilar <diegorubilartagle@gmail.com>
2019-07-16 20:51:51 -0400

    fix assets static image of meta tag

--- pages/index.vue
+++ pages/index.vue
@@ -33,14 +33,14 @@ export default {
       { property:"og:url", content:"https://www.mindgrowth.cl/" },
       { property:"og:title", content:"MindGrowth - Planificación Estratégica"},
       { property:"og:description", content:"Más que una consultora, MindGrowth representa un aliado estratégico para tu organización. Te ayudamos a planificar tu negocio para llevarlo al siguiente nivel."},
-      { property:"og:image", content:"~/assets/images/3x/card.jpg"},
+      { property:"og:image", content:"~/assets/card.jpg"},
 
       //twitter
       { property:"twitter:card", content:"summary_large_image"},
       { property:"twitter:url", content:"https://www.mindgrowth.cl/"},
       { property:"twitter:title", content:"MindGrowth - Planificación Estratégica"},
       { property:"twitter:description", content:"Más que una consultora, MindGrowth representa un aliado estratégico para tu organización. Te ayudamos a planificar tu negocio para llevarlo al siguiente nivel."},
-      { property:"twitter:image", content:"~/assets/images/3x/card.jpg"}
+      { property:"twitter:image", content:"~/assets/card.jpg"}
     
     ]
   }
