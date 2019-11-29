ad812fca3dc7671bd365547d6dc2e14e404ef7cf
Diego Rubilar <diegorubilartagle@gmail.com>
2019-11-29 16:02:29 -0300

    fix blog in navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -35,8 +35,8 @@
             <a class="nav-link" href="https://mindgrowth.typeform.com/to/oq0rOo" target="_blank">Diagnóstico <span href="#" class="badge badge-success">Gratis</span></a>
           </li>
           <li class="nav-item">
-            <a class="text-muted" href="https://blog.mindgrowth.cl" target="_blank">Blog</a>
-            </li>
+            <a class="nav-link" href="https://blog.mindgrowth.cl" target="_blank">Blog</a>
+          </li>
         </ul>
         <form class="ml-auto">
           <a href="https://www.facebook.com/mindgrowthcl" target="_blank" rel="noopener"><img class="logo-rrss" src="~/assets/images/svg/icons/facebook.svg" alt="facebook mindgrowthcl" /></a>
